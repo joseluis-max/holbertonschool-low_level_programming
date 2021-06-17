e0fcb8168bb3a53d4b5f25b503abe3ef9bb2835a
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-17 17:53:29 -0500

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print string with printf
+ *
+ * Return: String with printf
+ */
+int main()
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
