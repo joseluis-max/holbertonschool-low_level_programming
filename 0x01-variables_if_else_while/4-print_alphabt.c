619e1ce0b5d26c176abf1c9ff7e1939aafe394e2
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-18 20:54:50 -0500

    Alphabet with letter q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Alphabet
+ *
+ * Return: 0;
+ */
+int main(void)
+{	int i = 97;
+	int j = 65;
+
+	while (i <= 122)
+	{
+		if (i != 101 || i != 113  )
+			putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
