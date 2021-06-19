fbee6799a5a46f6374ea1cb2a7dae712f771bc4f
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-18 21:28:02 -0500

    base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Numbers base16
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 97;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j < 103)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
