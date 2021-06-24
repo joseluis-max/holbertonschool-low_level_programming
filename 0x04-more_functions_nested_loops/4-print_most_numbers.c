98d88b938b78790bc8f8bd6ba687d1ee18fad82b
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-24 14:35:36 -0500

    mos numbers again

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* print_numbers - function that prints the numbers, from 0 to 9.
+*/
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
