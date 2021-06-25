bf32e918a2f8963251c1283e14745ad6f3c71750
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-24 21:04:41 -0500

    lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print-diagonal - diagonal print
+ * @n: inter number
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int j = 0;
+		while (j < n)
+		{
+			int i = 0;
+			while (i <= (j + 1))
+			{
+				_putchar(32);
+				i++;
+			}
+			_putchar(92);
+			_putchar('\n');
+			j++;
+		}
+	}
+}
