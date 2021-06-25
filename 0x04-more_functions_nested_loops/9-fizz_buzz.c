038a6c4e5e8c0f0c39e50310195ad126c6b61ebf
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-24 22:18:36 -0500

    Multiples

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - multiples fizz and buzz
+ *
+ * Return: always 0;
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n < 101)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("%s ", "FizzBuzz");
+		}
+		else if (n % 3 == 0 && n % 5 != 0)
+		{
+			printf("%s ", "Fizz");
+		}
+		else if (n % 5 == 0 && n % 3 != 0)
+		{
+			printf("%s ", "Buzz");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
