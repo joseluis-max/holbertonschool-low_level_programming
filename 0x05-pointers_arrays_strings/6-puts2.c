dc0ee58e28d1188fe5d3ce10d8b53e90547d9a5b
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-28 18:27:24 -0500

    test

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,14 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
 	{
-		if (*str % 2 == 0)
-		{
-			_putchar(*str);
-		}
-		str++;
+		if (i % 2 == 0)
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
