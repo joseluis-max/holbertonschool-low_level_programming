f3c24c8ac7e5e2a5431302d92e1fdfae353ec22e
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-28 18:13:00 -0500

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * puts2 - print every other character
+ * @str: string for print
+ */
+void puts2(char *str);
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
