20a77305162636c91b08af0a8f0853ee6e5e39b3
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-06-28 20:19:54 -0500

    odd numbers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,8 +12,15 @@ void puts_half(char *str)
 	{
 		c++;
 	}
-	n = (c - 1) / 2;
-	while (n < c - 1)
+	if (c % 2 == 0)
+	{
+		n = c / 2;
+	}
+	else
+	{
+		n = (c - 1) / 2;
+	}
+	while (n < c)
 	{
 		_putchar(str[n]);
 		++n;
