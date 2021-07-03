fdeca2604f6415dd05c8f55e48bb787a4d07b5fa
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-02 19:00:39 -0500

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcat - concat two strings
+ * @dest: string for add
+ * @src: string where add
+ * Return: dest pointer new string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i])
+	{
+		if (!dest[i] )
+		{
+			dest[i] = src[j];
+			j++;
+		}
+		i++;
+	}
+	_putchar(i);
+	return dest;
+}
