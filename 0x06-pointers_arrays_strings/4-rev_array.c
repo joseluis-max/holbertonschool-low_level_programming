b1c09cde635d151fd556fcd0944d58bb9701cacb
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-02 21:54:54 -0500

    reverse string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * reverser_array - reverse array
+ * @a: array of integers
+ * @n: long of a
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tmp[12];
+
+	while (i < n)
+	{
+		tmp[i] = a[i];
+		i++;
+	}
+	i = 0;
+	while (n > 0)
+	{
+		a[n - 1] = tmp[i];
+		n--;
+		i++;
+	}
+}
