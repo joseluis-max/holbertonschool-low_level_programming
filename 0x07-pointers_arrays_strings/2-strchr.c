bc36b112304fcb82c9dc3b9166acf02412906d6d
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-05 14:51:21 -0500

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * * _strchr - locate character in string
+ * @s: string
+ * @c: character
+ * Return: first occurrence or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i))
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return ('\0');
+}
