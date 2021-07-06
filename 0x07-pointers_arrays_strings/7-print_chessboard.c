14c2364e5d163daca2ed222200ba369a77dacec1
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-05 19:26:46 -0500

    chessboard fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,9 +5,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	for (int i = 0;i < 8; i++)
+	int i, j;
+
+	for (i = 0; i < 8; i++)
 	{
-		_putchar(a[i]);
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+	_putchar('\n');
 	}
-	_putchar(32);
 }
