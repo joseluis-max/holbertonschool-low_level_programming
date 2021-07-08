d68cf38d08d4d92143cbf52088872259c2f15463
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-07 22:48:16 -0500

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - natural square root
+ * @n: number integer
+ * Return: natural square root or -1
+ */
+int counter(int i, int n)
+{
+	if (i * i == n)
+		return (i)
+	i++;
+}
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (0);
+	}
+	counter(0, n);
+	return (_sqrt_recursion(n));
+}
