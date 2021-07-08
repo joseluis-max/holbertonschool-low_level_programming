6a1406d60124d69708d15e8c60a6f3bc894d77a5
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-07 23:25:37 -0500

    sqrt and first is prime

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,32 @@
 #include "holberton.h"
-/**
- * _sqrt_recursion - natural square root
- * @n: number integer
- * Return: natural square root or -1
- */
-int _check_integer(int i, int n)
-{
-	if (i * n != n)
-	{
-		i++;
-	}
-	return (i);
-}
+int actual_sqrt_recursion(int n, int i);
 
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
-		return (0);
-	else
-	{
-		_check_integer(0, n);
-		return (_sqrt_recursion(n));
-	}
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * is_prime_number - prime number
+ * @n: int number
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if(n % )
+}
