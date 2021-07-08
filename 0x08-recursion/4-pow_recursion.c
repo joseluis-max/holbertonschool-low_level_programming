099d3044ff64450e4b12342346ad243b580ae8ec
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-07 21:31:35 -0500

    pow with recursion fixed

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "holbeton.h"
+#include "holberton.h"
 /**
  * _por_recursion - por with recursion
  * @x: raised
@@ -10,6 +10,10 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 	{
 		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
 	}
 	if (y == 1)
 	{
