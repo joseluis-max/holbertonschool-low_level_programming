139a4cbd3f2906faa24b3e7302c1c1218f6f7793
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-07 21:14:03 -0500

    factorial n

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * factorial - factorial of int
+ * @n: integer number
+ * Return: n < 0 -> -1, factorial of n
+ */
+int factorial (int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
