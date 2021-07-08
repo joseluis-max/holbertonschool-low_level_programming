1ebf33ed638bab9972e74be2e1f912bbc42c1493
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-07 21:24:40 -0500

    por with recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "holbeton.h"
+/**
+ * _por_recursion - por with recursion
+ * @x: raised
+ * @y: power
+ * Return: pow
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 1)
+	{
+		return (x);
+	}
+	return (x * _pow_recursion(x , y - 1));
+}
