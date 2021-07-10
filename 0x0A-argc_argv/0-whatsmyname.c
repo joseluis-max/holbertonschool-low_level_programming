111436ffdd1d8671f90c220408c4e7a04eecb914
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-09 19:40:09 -0500

    betty styles

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * whatsmyname - return the name of the program
- * argc: length of arguments
- * arfv: vector with the arguments
+ * main - return the name of the program
+ * @argc: length of arguments
+ * @argv: vector with the arguments
+ * Return: 0 succesfully or 1 error
  */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
-}	
+}
