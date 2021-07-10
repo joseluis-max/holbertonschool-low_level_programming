e6d28dafa648bb59c47815d3674fa449cb81a2e8
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-09 19:54:31 -0500

    Number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/** main - length of arguments
+ * @argc: length of arguments
+ * @argv: arguments vector
+ * Return: 1 or 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
