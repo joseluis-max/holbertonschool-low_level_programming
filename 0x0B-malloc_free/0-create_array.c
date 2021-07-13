1d10d80993c88ec137b96f0d2592ffe42b97b2d9
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-13 16:59:57 -0500

    with every byte allocated with pointer

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stddef.h>
 /**
  * create_array - create an array with specific strin
  * @size: lenght of array
@@ -9,8 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	int i = 0;
-	char *str = malloc(sizeof(char) * size);
+        char *str = malloc(sizeof(char) * size);
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	while (str)
 	{
 		*(str + i) = c;
