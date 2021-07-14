110d0acc21fc6acb954c21270457f9d838210beb
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-13 23:17:48 -0500

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,8 +35,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		s[j] = s1[j];
 	}
-	for (l = i, j = 0; l < k; l++, j++)
+	j = 0;
+	for (l = i; l < k; l++)
 		s[l] = s2[j];
+		j++;
 	s[l] = '\0';
 	return (s);
 }
