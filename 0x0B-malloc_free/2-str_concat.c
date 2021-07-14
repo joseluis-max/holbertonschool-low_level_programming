c1ba4253e292a3bf6a3cdc41fb8bc8610c4b65c0
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-13 23:10:15 -0500

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		return (s1);
 	if (s1 == NULL && s2 == NULL)
-		return ("");
+		return ('\0');
 	while (s1[i] != '\0')
 		i++;
 	while (s2[k] != '\0')
