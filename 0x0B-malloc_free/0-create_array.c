34df31844e61a3b04d0676c5a6b8c3bc0a79d272
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-13 20:52:22 -0500

    betty styles

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,8 +10,8 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-        char *str = malloc(sizeof(char) * size);
-	
+	char *str = malloc(sizeof(char) * size);
+
 	if (size == 0 || str == NULL)
 	{
 		return (NULL);
