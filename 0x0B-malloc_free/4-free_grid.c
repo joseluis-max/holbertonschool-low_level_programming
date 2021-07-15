f87fb55a5f6fc707bbf141839b057f1b8f55c11f
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-14 21:14:13 -0500

    free function betty styles

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,13 +5,13 @@
  * @grid: 2D array
  * @height: rows number
  */
-void free_grid( int **grid, int height)
+void free_grid(int **grid, int height)
 {
 	int row;
 
 	for (row = 0; row < height; row++)
 	{
-			free(grid[row]);
+		free(grid[row]);
 	}
 	free(grid);
 }
