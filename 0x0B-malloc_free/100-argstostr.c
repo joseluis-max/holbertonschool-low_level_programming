8a5599b5742c36631ce57f88f4379f1b95f45231
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-14 21:42:42 -0500

    arguments to str

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - concat all arguments
+ * @ac: argument counter
+ * @av: arguments 2D array
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int row;
+	int col;
+	int c;
+	int i;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (row = 0; row < ac - 1; row++)
+	{
+		for (col = 0; av[row][col] != '\0'; col++)
+		{
+			c++;
+		}
+	}
+	str = malloc(sizeof(char) * (c + ac + 1));
+	if (p == NULL)
+		return (NULL);
+	for (row = 0; row < ac - 1; row++)
+        {
+                for (col = 0; av[row][col] != '\0'; col++)
+                {
+                        str[i] = av[row][col];
+			i++;
+                }
+		str[i] = '\n'
+		i++;
+        }
+	return (str);
+
+}
