bd1be64031f64ada9af4caec6145a5f492646525
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-16 22:36:03 -0500

    array range library definition

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,19 +11,20 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int i;
-	int m = min;
+	int n = min;
+	int size = (max - min) + 1;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int) * (max - min) + 1);
+	arr = malloc(sizeof(int) * size);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < (max - min) + 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		arr[i] = m;
-		m++;
+		arr[i] = n;
+		n++;
 	}
 	return (arr);
 }
