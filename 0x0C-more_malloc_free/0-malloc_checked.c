4ec5ec009e75c6c681c3ae57677d40bf9bffd720
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-16 20:00:36 -0500

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - ckecked allocation
+ * @b: size of allocation
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *b;
+	
+	b = malloc(b);
+	if (b = NULL)
+	{
+		exit(98);
+	}
+	return (b);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, unsigned int n);
+void *_calloc(unsigned int nmeb, unsigned int size);
+int *array_range(int min, int max);
+
