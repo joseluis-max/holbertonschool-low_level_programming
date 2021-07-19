500a999afbe68f352d599f31af18492401eac663
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-19 15:04:30 -0500

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,11 +11,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *my_dog;
+	int i = 0;
+	int j = 0;
+	int k;
+	int l;
+	char *n;
+	char *o;
+
+	while (name[i])
+		i++;
+	while (owner[j])
+		j++;
+	n = malloc(sizeof(char) * i + 1);
+	for (k = 0; k < i; k++)
+		n[k] = name[k];
+	n[k] = '\0';
+	o = malloc(sizeof(char) * j + 1);
+	for (l = 0; l < j; l++)
+		o[l] = owner[l];
+	o[l] = '\0';
+
 	my_dog = malloc(sizeof(struct dog));
 	if (my_dog == NULL)
 		return (NULL);
-	my_dog->name = name;
+	my_dog->name = n;
 	my_dog->age = age;
-	my_dog->owner = owner;
+	my_dog->owner = o;
 	return (my_dog);
 }
