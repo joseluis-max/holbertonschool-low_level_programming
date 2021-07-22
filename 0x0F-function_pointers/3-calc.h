ae1e5913e6639ce2cd45da1b39958f40273feed0
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-21 22:39:13 -0500

    calc betty

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,5 @@
 #ifndef CALCH
 #define CALCH
-
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
@@ -15,8 +14,7 @@ int (*get_op_func(char *s))(int, int);
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
-
 #endif
