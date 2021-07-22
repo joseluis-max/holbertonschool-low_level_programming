956a6a74134da71c7b21d80a6065e92e97a8d3fe
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-07-21 20:21:51 -0500

    add print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * print_name - print a name with a pointer function
+ * @name: name for imprime
+ * @f:pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL)
+		f(name);
+}
