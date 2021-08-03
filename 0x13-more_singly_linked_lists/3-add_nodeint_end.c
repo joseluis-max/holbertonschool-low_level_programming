b74bc9782f0cc3bf5d4f7bb654ad696889e2dcfa
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-08-03 09:33:55 -0500

    n null

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *node, *h;
 
 	node = malloc(sizeof(listint_t));
-	if (node == NULL)
+	if (node == NULL == n == NULL)
 	{
 		free(node);
 		return (NULL);
