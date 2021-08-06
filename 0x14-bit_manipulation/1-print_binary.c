7bcf0cde84062092d9507be4bf58d46cb2cb34a5
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-08-05 21:00:28 -0500

    fixing sintaxis

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,19 +6,17 @@
  * @n:decimal number
  * Description: prints the binary representation of a number
  */
-
 void print_binary(unsigned long int n)
 {
-	int start = 0;
-	int i =  sizeof(n) * 8;
+	int i, flag;
 
-	for (i; i > 0; i--)
+	if (n == 0)
+		_putchar('0');
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
 		if ((n >> i) & 1)
-		{
-			start = 1;
-		}
-		if (start)
+			flag = 1;
+		if (flag == 1)
 			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
 }
