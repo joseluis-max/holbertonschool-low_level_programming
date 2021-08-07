2960298b69fca74964c94f9574c1360bdfe2d48d
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-08-07 13:23:56 -0500

    close open file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,16 +20,15 @@ int create_file(const char *filename, char *text_content)
 		size++;
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_CREAT| O_WRONLY| O_TRUNC , 0600);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_descriptor != 3)
-	{
 		return (-1);
-	}
 	if (text_content != NULL)
 	{
 		status = write(file_descriptor, text_content, size);
 		if (status == -1)
 			return (-1);
 	}
+	close(file_descriptor);
 	return (1);
 }
