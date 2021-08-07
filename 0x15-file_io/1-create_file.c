2a36a6a92e254961f55387827e54fcbeac4fce2e
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-08-07 13:33:45 -0500

    fix the semegtation default on counter of string

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,6 @@ int create_file(const char *filename, char *text_content)
 	int size = 0;
 	int status;
 
-	while  (text_content[size])
-		size++;
 	if (filename == NULL)
 		return (-1);
 	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -25,6 +23,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
+		while (text_content[size])
+			size++;
 		status = write(file_descriptor, text_content, size);
 		if (status == -1)
 			return (-1);
