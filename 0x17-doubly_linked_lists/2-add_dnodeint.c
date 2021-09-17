2981bdae873bd6b0390a2760ab85254ecc919e2e
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-17 13:59:02 -0500

    head == NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *node;
 
+	if (head == NULL)
+		return (NULL);
 	node = malloc(sizeof(dlistint_t));
 	if (node == NULL)
 	{
@@ -18,7 +20,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	node->n = n;
 	node->prev = NULL;
-	node->next = *head;
+	node->next = NULL;
+	if (*head != NULL)
+		node->next = *head;
 	*head = node;
 	return (*head);
 }
