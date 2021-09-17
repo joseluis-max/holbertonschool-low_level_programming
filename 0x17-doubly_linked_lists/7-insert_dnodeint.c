8b80eb6edfa4bb5edda426ba36fe522aeb1bb1d3
Jose Luis <luisj.valdiviezo20@gmail.com>
2021-09-17 11:37:26 -0500

    add node in n-th index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * insert_dnodeint_at_index - insert node at index
+ * @h: pointer to head
+ * @idx: index
+ * @n: new value
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *copy;
+	dlistint_t *node;
+	unsigned int i = 0;
+
+	copy = *h;
+	if (copy == NULL)
+		return (NULL);
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		add_dnodeint(h, n);
+		return (*h);
+	}
+	node->n = n;
+	node->next = NULL;
+	node->prev = NULL;
+	while (copy)
+	{
+		if (i == idx)
+		{
+			node->prev = copy;
+			node->next = copy->next;
+			copy->next = node;
+			copy->next->prev = node;
+			return (*h);
+		}
+		if (copy->next == NULL && i == idx - 1)
+		{
+			add_dnodeint_end(h, n);
+			return (*h);
+		}
+		copy = copy->next;
+		i++;
+	}
+	return (NULL);
+}
