52dc9db3195fed19b7c7bed7747bae05f7c5879b
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-09 14:12:26 -0500

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+ * 
+ * 
+ * 
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+        hash_node_t *tmp;
+        unsigned long int i;
+        int flag = 0;
+
+        if (ht != NULL && ht->array != NULL)
+        {
+                printf("{");
+                for (i = 0; i < ht->size; i++)
+                {
+                        tmp = ht->array[i];
+                        if (tmp != NULL)
+                        {
+                                if (!flag)
+                                {
+                                        printf("'%s': '%s'", tmp->key, tmp->value);
+                                        flag = 1;
+                                }
+                                else
+                                        printf(", '%s': '%s'", tmp->key, tmp->value);
+                        }
+                }
+                printf("}\n");
+        }
+               
+}
