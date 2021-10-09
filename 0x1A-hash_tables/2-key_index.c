a6ec4ebcb8f4d56103b99e1374df54fb1169f913
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-09 16:33:58 -0500

    betty styles

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,11 +3,12 @@
  * key_index - Calculate index key on table
  * @key: string for add to hash table
  * @size: long of hash table
- * Return: index at which the key/value pair should be stored in the array of the hash table
+ * Return: index at which the key/value pair should
+ * be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-        unsigned long int hash = hash_djb2(key);
+	unsigned long int hash = hash_djb2(key);
 
-        return (hash % size);
+	return (hash % size);
 }
