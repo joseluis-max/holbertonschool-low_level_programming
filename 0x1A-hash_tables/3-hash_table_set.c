fb8fa004e5ed1405e6d531d0a87b70f73f7c3303
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-09 16:28:03 -0500

    betty styles

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (status);
 	}
-	
 	node->key = (char *)key;
 	node->value = strdup((char *)value);
 	node->next = NULL;
