1108d0d8db843d876c85dd7958884bbbf015115a
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-10 13:05:34 -0500

    hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *node = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || value == NULL || ht->array == NULL)
 		return (status);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
