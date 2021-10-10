8a62a34dd978b0a93f667cf3315854260f963a52
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-10-10 08:58:23 -0500

    search in linked list

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index = 0;
 
+	if (ht == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
 
 	while (ht->array[index] != NULL)
