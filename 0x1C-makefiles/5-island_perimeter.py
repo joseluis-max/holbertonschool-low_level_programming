4d2147b41cc77c888f16979d5688ef06179a9e67
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-29 15:51:53 -0500

    ADD: island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,6 +2,7 @@
 """Perimeter of the island described in grid
 """
 
+
 def island_perimeter(grid):
     """Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid:
 
