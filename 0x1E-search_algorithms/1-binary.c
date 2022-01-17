3d509177ee8afabe0ebeca2dc1851496353b5aba
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-17 15:40:51 -0500

    UPDATE: print array and fiz size bug

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,5 @@
 #include "search_algos.h"
 #include <math.h>
-
 /**
  * binary_search - searchs for a value in a sorted array of integers
  * @array: sorted list of integers
@@ -9,21 +8,32 @@
  * Return: return index on founded element or -1
  * if not found or array is NULL
  */
-
 int binary_search(int *array, size_t size, int value)
 {
 	int middle = 0;
-	int letf = 0;
+	int left = 0;
 	int rigth = size - 1;
+	int i = 0;
 
 	if (array == NULL)
 		return (-1);
-	while (letf <= rigth)
+	
+	while (left <= rigth)
 	{
-		middle = ceil(size / 2);
+		printf("Searching in array: ");
+		for (i = left; i <= rigth; i++)
+		{
+			if (i == rigth)
+				printf("%d\n", array[i]);
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+		}
+		middle = ceil((left + rigth) / 2);
 		if (array[middle] < value)
 		{
-			letf = middle + 1;
+			left = middle + 1;
 		}
 		else if (array[middle] > value)
 		{
