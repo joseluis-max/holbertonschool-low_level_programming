d96616de2623ae07f187fe3fa0aa8864fa96aced
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-17 13:45:32 -0500

    UDPATE: header import stddef and stdio, linear file return -1 in case of error and update doc fucntion

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,6 +1,4 @@
 #include "search_algos.h"
-#include <aio.h>
-#include <stdlib.h>
 
 /**
  * linear_search - search for a value in array
@@ -8,19 +6,23 @@
  * @value: element for found in array
  * @size: length of list of integers (array)
  * Return: first index where value is founded
- * or NULL if not is present
+ * or -1 if not is present or if array is NULL
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
 		{
-			printf("Found %d at index: %d", value, i);
+			printf("Found %d at index: %ld", value, i);
 			return (i);
 		}
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%ld] = [%d]", i, array[i]);
 	}
+	return (-1);
 }
