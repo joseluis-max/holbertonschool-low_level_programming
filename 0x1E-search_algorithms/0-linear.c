2e8ea6cfc2345b0e7f5d2c0a88da7558fa1d4c06
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-17 13:50:07 -0500

    UPDATE: print \n and don't print found sentenc

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,12 +17,11 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found %d at index: %ld", value, i);
 			return (i);
 		}
-		printf("Value checked array[%ld] = [%d]", i, array[i]);
 	}
 	return (-1);
 }
